e76ae8b2dd398b53b246e1f03fe3fd7d25e0f9a4
zixi <zixi@jingling.com>
2023-05-06 15:47:27 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="fd-window-box">
+   <div class="fd-window-box__context">
+    请问有什么可以帮您？
+    <template v-for="(item,index) in form.context"  :key="index">
+      <div style="white-space: pre-wrap;">
+        {{ item }}
+      </div>
+    </template>
+      
+    </div>
+   <div class="fd-window-box__input">
+     <el-input placeholder="请输入内容,按回车确认" v-model="form.options"   @keyup.enter="onEnter" />
+   </div>
+</div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+
+<script setup>
+import { ElLoading } from 'element-plus'
+ const config = {
+    organization: 'org-3f09XtCUjvIwyefCFdXqUoTg',
+    apiKey: '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',
   }
+  const options = {
+    model: 'text-davinci-003',
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: ['input:'],
+  }
+
+import { reactive } from 'vue'
+import axios from 'axios';
+const instance = axios.create({
+  baseURL: 'http://156.236.73.178:3005'
+})
+
+const form = reactive({
+  information:'',
+  context:''
+})
+
+const sendFormData = () => {
+  const lodaing = ElLoading.service({
+    lock: true,
+    text: 'Loading',
+    background: 'rgba(0, 0, 0, 0.7)',})
+  const data = { options: {...options,prompt:form.options},config };
+  instance.post("/api/createCompletion", data)
+    .then(response => {
+      const {choices} = response.data.data;
+      form.context = choices.map(item=>{
+      return item.text
+    })
+    lodaing.close()
+    })
+    .catch(error => {
+      lodaing.close()
+      console.error(error);
+    });
+}
+
+const onEnter = ()=>{
+
+  sendFormData();
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="less">
+.fd-window-box{
+  width: 500px;
+  margin: 0 auto;
+  padding: 15px;
+  &__context{
+   min-height: 200px;
+  border: 1px solid #be2222;
+  border-radius: 10px;
+  border: 1px solid #b3b3b3;
+  }
+  &__input{
+  margin-top: 20px;
+  }
 }
 </style>
